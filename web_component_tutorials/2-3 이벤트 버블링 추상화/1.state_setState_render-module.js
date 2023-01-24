b60939eaab5aac76327acf3076a491f1fe30b3fb
hoyun293 <hoyun293@naver.com>
2023-01-24 20:04:21 +0900

    Delete web_component_tutorials directory

--- web_component_tutorials/2-3 이벤트 버블링 추상화/1.state_setState_render-module.js
+++
@@ -1,9 +0,0 @@
-import Items from './Item.js';
-class App {
-    constructor() {
-        const $app = document.querySelector('#app');
-        new Items($app);
-    }
-}
-
-new App();
--- web_component_tutorials/3-1.컴포넌트 분할하기 기능구현/1.state_setState_render-module.js
+++
@@ -1,9 +0,0 @@
-import Items from './item.js';
-class App {
-    constructor() {
-        const $app = document.querySelector('#app');
-        new Items($app);
-    }
-}
-
-new App();
